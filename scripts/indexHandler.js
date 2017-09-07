f6218bee05ea06a7375e49bbf11c4a0ecf0955e4
Remste <remi.r.stevens@gmail.com>
2017-09-07 22:44:11 +0100

    t

--- scripts/indexHandler.js
+++ scripts/indexHandler.js
@@ -1,24 +1,4 @@
-// var Remste = Remste || {};
-// Remste.GitHub = Remste.GitHub || {};
-
-// Remste.GitHub.Index = function () {
-    
-//         return {
-    
-//             // Public functions and variables
-    
-//             initialise: function () {
-                
-                
-                
-//             }
-//         };
-    
-//     }();
-    
 $(document).ready(function () {
-    // Remste.GitHub.Index.initialise();
-    alert("hi");
     $("#headerContent").load("../_includes/Header.html"); 
     $("#footerContent").load("../_includes/Footer.html"); 
 });
